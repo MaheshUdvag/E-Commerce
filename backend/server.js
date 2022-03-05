d0b37c97453bfac224a6b074b7287e1faa5273b5
MaheshUdvag <maheshudvag@gmail.com>
2022-03-05 21:11:26 +0530

    updated build path

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ app.use("/api/users", UserRoutes);
 app.use("/api/orders", OrderRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../frontend/build"));
+  app.use(express.static("./build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve("../frontend", "build", "index.html"));
+    res.sendFile(path.resolve("./build", "index.html"));
   });
 }
 
