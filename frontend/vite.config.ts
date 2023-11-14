7dcda884e383a69423cd639156c5b19fb5302f70
maheshudvag <maheshudvag@gmail.com>
2023-11-14 16:07:32 +0530

    updated vite env config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-      },
-    },
-  },
-})
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    const config = {
+        base: '/',
+        plugins: [react()],
+        server: {
+          proxy: {
+            '/api': {
+              target: process.env.VITE_API_HOST,
+              changeOrigin: true,
+            },
+          },
+        },
+      }
+
+    return defineConfig(config);
+};
